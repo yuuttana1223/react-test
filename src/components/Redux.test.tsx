2a5d7dddaa77abb8837e0892d4b45f7d3d668f10
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-05-21 22:27:15 +0900

    ♻️ renderを自作して使用

--- src/components/Redux.test.tsx
+++ src/components/Redux.test.tsx
@@ -1,28 +1,10 @@
-import { screen, render } from "@testing-library/react";
 import { Redux } from "./Redux";
+import { render, screen } from "../utils/test-utils";
 import userEvent from "@testing-library/user-event";
-import { AnyAction, configureStore, Store } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import { customCounterReducer } from "../features/customCounter/customCounterSlice";
 
 describe("Redux Integration Test", () => {
-  // eslintの予測 anyは仕方がない
-  let store: Store<any, AnyAction>;
-  // 毎回storeを作り直したい
-  beforeEach(() => {
-    store = configureStore({
-      reducer: {
-        customCounter: customCounterReducer,
-      },
-    });
-  });
-
   it("Should display value with increment by 1 per click", async () => {
-    render(
-      <Provider store={store}>
-        <Redux />
-      </Provider>
-    );
+    render(<Redux />);
     const PLUS_COUNT = 3;
     for (let i = 0; i < PLUS_COUNT; i++) {
       await userEvent.click(screen.getByRole("button", { name: "+" }));
@@ -31,11 +13,7 @@ describe("Redux Integration Test", () => {
   });
 
   it("Should display value with decrement by 1 per click", async () => {
-    render(
-      <Provider store={store}>
-        <Redux />
-      </Provider>
-    );
+    render(<Redux />);
     const MINUS_COUNT = -2;
     for (let i = 0; i > MINUS_COUNT; i--) {
       await userEvent.click(screen.getByRole("button", { name: "-" }));
@@ -44,11 +22,7 @@ describe("Redux Integration Test", () => {
   });
 
   it("Should display value with incrementByAmount", async () => {
-    render(
-      <Provider store={store}>
-        <Redux />
-      </Provider>
-    );
+    render(<Redux />);
     await userEvent.type(screen.getByPlaceholderText("Enter"), "10");
     await userEvent.click(
       screen.getByRole("button", { name: "IncrementByAmount" })
---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,25 @@
+import { render as rtlRender } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { customCounterReducer } from "../features/customCounter/customCounterSlice";
+import { ReactElement } from "react";
+
+function render(
+  ui: ReactElement,
+  {
+    store = configureStore({
+      reducer: { customCounter: customCounterReducer },
+    }),
+    ...renderOptions
+  } = {}
+) {
+  const Wrapper = ({ children }: { children: ReactElement }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from "@testing-library/react";
+// override render method
+export { render };
